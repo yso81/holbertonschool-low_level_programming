31174c08d4313fd7361075147925ed2d2b6e89cc
Yannick <yannicksookree@gmail.com>
2025-06-30 14:09:19 +0400

    makeitbettycompliant

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper - checks for an uppercase character
- * @c: The character to be checked (passed as an int)
+ * _isupper - checks for an uppercase character.
+ * @c: The character to be checked.
  *
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise.
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-       	{
+	{
 		return (1);
 	}
 	else
 	{
 		return (0);
-    }
+	}
 }

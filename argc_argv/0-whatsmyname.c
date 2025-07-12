8d8a0a0c673955c28793298386163979b6a81f56
Yannick <yannicksookree@gmail.com>
2025-07-12 16:48:29 +0400

    a program that prints its name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,6 +6,9 @@
  * Description: A program that prints its own name followed by a new line.
  * @argc: Number of command line argumrnts.
  * @argv: An array of strings containing the arguments.
+ *
+ * Return: Always 0 (Success)
+ *
  */
 
 int main(int argc, char *argv[])

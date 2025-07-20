48d2ad7aadc0188cbd58e24c34bf234bcd622b9f
Yannick <yannicksookree@gmail.com>
2025-07-20 13:21:53 +0400

    new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure that defines the properties of a dog.
+ * @name: Pointer to a character string for the dog's name.
+ * @age: The age of the dog as a floating-point number.
+ * @owner: Pointer to a character string for the owner's name.
+ *
+ * Description: This structure is used to store information about a dog,
+ * including its name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
